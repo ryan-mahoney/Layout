f2d9fd82e3ffa1da3176d4e1f16cb03037ecbf02
webuser <ryan@virtuecenter.com>
2013-08-26 22:56:03 -0400

    Initial commit

---
+++ example-sep.js
@@ -0,0 +1,10 @@
+$(function() {
+	$.separation([
+		{
+			'jsonUrl': "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+			'args': {tags: "mount rainier", tagmode: "any", format: "json"},
+			'template': './flickr.hbs',
+			'selector': 'body'
+		}
+	]);
+});
---
+++ jquery.separation.js
@@ -0,0 +1,23 @@
+(function ($) {
+    $.fn.separation = function (config) {
+        console.log(config);
+        $(config).each(function (offset, partial) {
+            "use strict";
+            if (partial.jsonUrl === 'undefined' || partial.templateName === 'undefined' || partial.selector === 'undefined') {
+                if (console) {
+                    console.log('Skipped partial due to missing parameter, check jsonUrl, or templateName, or selector');
+                    return;
+                }
+            }
+            $.getJSON(partial.jsonUrl, partial.args).done(function (data) {
+                $.ajax({
+                    url: partial.template,
+                    success: function (src) {
+                        $(partial.selector).html(Handlebars.compile(src)(data));
+                    },
+                    dataType: 'text'
+                });
+            });
+        });
+    };
+}(jQuery));

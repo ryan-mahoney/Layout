30dbe5c0a96d1d8038f1775936c5acd4093dbb91
webuser <ryan@virtuecenter.com>
2013-09-02 21:19:43 -0400

    cleanup

--- example/layouts/blogs-sep.js
+++ example/layouts/blogs-sep.js
@@ -4,7 +4,7 @@ $(function() {
 			"id": "blogs",
 			"url": "http://collections.localhost/json/blogs/all/1/4",
 			"args": {},
-			"hbs": "https://raw.github.com/virtuecenter/separation/master/example/templates/blogs.hbs",
+			"hbs": "../templates/blogs.hbs",
 			"selector": "#blogs",
 			"type": "Collection"
 		}

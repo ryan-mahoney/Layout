45e2a17f57c436e0711c961f9407170d626d6ace
webuser <ryan@virtuecenter.com>
2013-09-04 05:47:32 -0400

    testing PHP version

---
+++ example/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once('../Separation.php');
+Separation::config([
+	'layouts' 		=> __DIR__ . '/layouts/',
+	'templates' 	=> __DIR__ . '/templates/',
+	'sep'			=> __DIR__ . '/sep/'
+]);
+Separation::layout('blogs.html')->template()->write();

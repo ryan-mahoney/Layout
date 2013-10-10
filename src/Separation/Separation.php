213eb2bc9ed5cfc7807628c68f8cf293321b4869
webuser <ryan@virtuecenter.com>
2013-10-09 23:17:37 -0400

    remove debugging code

--- src/Separation/Separation.php
+++ src/Separation/Separation.php
@@ -114,8 +114,6 @@ class Separation {
 					$dataUrl = self::collectionUrl($entity);
 				} elseif ($entity['type'] == 'Document') {
 					$dataUrl = self::documentUrl($entity);
-					echo $dataUrl;
-					exit;
 				}
 			}
 			if (isset($entity['cache'])) {

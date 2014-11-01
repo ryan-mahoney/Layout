32a1b98c2d2d4750443b605053a74b1ba888f56e
webuser <ryan@virtuecenter.com>
2014-10-31 23:35:36 -0400

    naming

--- src/Layout/Model.php
+++ src/Layout/Model.php
@@ -4,20 +4,20 @@ namespace Opine\Layout;
 class Model {
 	private $root;
 	private $layout;
-	private $bundleRoot;
+	private $bundleModel;
 	private $cache;
 
-	public function __construct ($root, $layout, $bundleRoot, $cache) {
+	public function __construct ($root, $layout, $bundleModel, $cache) {
 		$this->root = $root;
 		$this->layout = $layout;
-		$this->bundleRoot = $bundleRoot;
+		$this->bundleModel = $bundleModel;
 		$this->cache = $cache;
 	}
 
 	public function build () {
 		$apps = [];
 		$apps[] = $this->root . '/../app';
-		$bundles = $this->bundleRoot->bundles();
+		$bundles = $this->bundleModel->bundles();
 		foreach ($bundles as $bundles) {
 			$apps[] = $this->root . '/../bundles/' . $bundle['name'] . '/app';
 		}
